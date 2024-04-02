a1f6a549bf1b4498a37eda229f6ac0bf184a43d3
sseohalee <seohalee0131@gmail.com>
2024-04-02 16:05:57 +0900

    feat: jpa test 예제 답안 수정

--- Ex12H2DBTest/src/main/java/com/study/Ex12H2DB/MemberRepository.java
+++ Ex12H2DBTest/src/main/java/com/study/Ex12H2DB/MemberRepository.java
@@ -1,10 +1,10 @@
 package com.study.Ex12H2DB;
 
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -39,28 +39,47 @@ public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
             String userid, String username
     );
 
-    Boolean existsByJoindateLessThanEqual(LocalDate date); // boolean이면 exists로 시작
-    long countByUserNameIgnoreCaseLike(String userid); // long은 count로 시작
-
-    // JPA에서 SQL을 사용하는 방법
-    // 1. JPQL
-    //  - 표준 ANSI SQL 문법을 지원함.
-    //  - 특정 데이터베이스에 종속적인 기능은 지원하지 않음.
-    //  - from절 뒤에는 entity클래스이름을 넣어준다. (소문자로 하면 에러)
-    @Query(value="SELECT m FROM MemberEntity m WHERE m.userId = :userid")
-    List<MemberEntity> findByUserId_JPQL_Query(String userid);
-
-    // 2. Native SQL
-    //  - 특정 데이터베이스에 종속적인 기능을 제공한다.
-    //    예) MySQL : LIMIT 5, now(), AUTO_INCREMENT
-    //        Oracle : sysdate, 시퀀스
-    //      1. Update, Insert, Delete 문은 @Modifying, @Transactional을 추가해야 됨.
-    @Query(value = "SELECT * FROM member WHERE user_id = :userid", nativeQuery = true)
-    List<MemberEntity> findByUserId_nativeQuery(String userid);
+    // 연습문제 ----------------------
+    Boolean existsByUserPw(String userpw);
+    Boolean existsByUserId(String userid);
+    @Query(value = "SELECT * FROM member WHERE joindate BETWEEN '2023-03-01' AND '2023-03-31'",
+            nativeQuery = true)
+    List<MemberEntity> findByJoindate();
+    Boolean existsByJoindateBetween(LocalDate start, LocalDate end);
 
     @Modifying
     @Transactional
-    @Query(value = "UPDATE member SET user_id = :userid where id = id",
-           nativeQuery = true )
-    int updateById_nativeQuery(Long id);
+    @Query(value = "UPDATE member SET user_name = :username, user_pw = :userpw where user_id = :userid",
+            nativeQuery = true)
+    int updatePractice(String userid, String username, String userpw);
+
+    // -----------------------------
+
+    Boolean existsByJoindateLessThanEqual(LocalDate date);
+    long countByUserNameIgnoreCaseLike(String userid);
+
+    //JPA에서 SQL을 사용하는 방법
+    //1. JPQL
+    // - 표준 ANSI SQL 문법을 지원함.
+    // - 특정 데이터베이스에 종속적인 기능은 지원하지 않음.
+    // - from절 뒤에는 엔티티 클래스이름을 넣어준다.(소문자로 하면 에러)
+    @Query(value="SELECT m FROM MemberEntity m WHERE m.userId = :userid")
+    List<MemberEntity> findByUserId_JPQL_Query(String userid);
+
+    //2. Native SQL
+    // - 특정 데이터베이스에 종속적인 기능을 제공한다.
+    //   예) MySQL : LIMIT 5, now(), AUTO_INCREMENT
+    //      Oracle : sysdate, 시퀀스
+    // Update,Insert,Delete문은 @Modifying, @Transactional을 추가해야 됨.
+    @Query(value = "SELECT * FROM member WHERE user_id = :userid",
+            nativeQuery = true)
+    List<MemberEntity> findByUserId_nativeQuery(String userid);
+
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE member SET user_id = :userid where id = :id",
+            nativeQuery = true)
+    int updateById_nativeQuery(Long id, String userid);
+
 }
--- Ex12H2DBTest/src/test/java/com/study/Ex12H2DB/MemberRepositoryTest.java
+++ Ex12H2DBTest/src/test/java/com/study/Ex12H2DB/MemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.study.Ex12H2DB;
 
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.lang.reflect.Member;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -15,6 +17,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
+//@RequiredArgsConstructor
 public class MemberRepositoryTest extends Ex12H2DbApplicationTests {
     //테스트 클래스에서는 생성자 주입 안됨.
     @Autowired
@@ -33,13 +36,13 @@ public class MemberRepositoryTest extends Ex12H2DbApplicationTests {
     @DisplayName("save 테스트") //콘솔에 출력되는 메소드 이름
     public void save(){
         memberRepository.save(new MemberEntity(Long.valueOf(1),
-                "hong", "1234", "홍길동", "ROLE_USER", LocalDate.now()
+                "hong", "1234", "홍길동", "ROLE_USER", LocalDate.parse("2023-01-01")
         ));
         memberRepository.save(new MemberEntity(Long.valueOf(2),
-                "tom", "1234", "톰아저씨", "ROLE_USER", LocalDate.now()
+                "tom", "1234", "톰아저씨", "ROLE_USER", LocalDate.parse("2023-02-01")
         ));
         memberRepository.save(new MemberEntity(Long.valueOf(3),
-                "admin", "1234", "관리자", "ROLE_ADMIN", LocalDate.now()
+                "admin", "1234", "관리자", "ROLE_ADMIN", LocalDate.parse("2023-03-01")
         ));
 
         List<MemberEntity> list = memberRepository.findAll();
@@ -51,6 +54,60 @@ public class MemberRepositoryTest extends Ex12H2DbApplicationTests {
         //assertEquals( 3, list.size() );
         //assertEquals( 4, list.size() );
     }
+
+    // 연습문제 ---------------------------
+    @Test
+    @DisplayName("연습1")
+    public void findByPw(){
+        boolean isExist = memberRepository.existsByUserPw("1234");
+        assertEquals(isExist, true);
+    }
+
+    @Test
+    @DisplayName("연습2")
+    public void findByDate(){
+        List<MemberEntity> list =
+                memberRepository.findByJoindate();
+        assertEquals(1, list.size());
+    }
+
+    @Test
+    @DisplayName("연습2-1")
+    public void Pr02(){
+        LocalDate start = LocalDate.of(2023, 3, 1);
+        LocalDate end = LocalDate.of(2023, 3, 31);
+        boolean isExists = memberRepository.existsByJoindateBetween(start, end);
+        assertEquals(isExists, true);
+    }
+
+    @Test
+    @DisplayName("연습3")
+    public void dupl(){
+        boolean isExist = memberRepository.existsByUserId("lee");
+        assertEquals(isExist, false);
+    }
+
+    @Test
+    @DisplayName("연습4")
+    public void updatePractice(){
+        int result = memberRepository.updatePractice("tom", "마이클", "3456");
+        assertEquals(1, result);
+
+        Optional<MemberEntity> member =
+                memberRepository.findById(2L);
+        assertEquals("마이클", member.get().getUserName() );
+
+        List<MemberEntity> list = memberRepository.findByUserId("tom");
+        if(list!=null && list.size()>0){
+            assertEquals("마이클", list.get(0).getUserName());
+            assertEquals("3456", list.get(0).getUserPw());
+        } else{
+            System.out.println("null이거나 size 0 입니다.");
+        }
+    }
+
+
+    // ----------------------------------
     @Test
     @DisplayName("findById 테스트")
     public void findById(){
@@ -67,6 +124,11 @@ public class MemberRepositoryTest extends Ex12H2DbApplicationTests {
         }else{
             fail("member 값 가져오기 실패");
         }
+        member.ifPresentOrElse((unwrappedMemberEntity) -> {
+            // null이 아닐 때 수행되는 람다식
+            System.out.println(unwrappedMemberEntity.getUserName());
+            assertEquals("홍길동", unwrappedMemberEntity.getUserName());
+        }, () -> fail("member 엔티티가 null입니다."));
     }
     @Test
     @DisplayName("count 테스트")
@@ -85,35 +147,38 @@ public class MemberRepositoryTest extends Ex12H2DbApplicationTests {
         assertEquals( 1, list.size() );
         assertEquals( "톰아저씨", list.get(0).getUserName() );
 
-//        List<MemberEntity> list2 =
-//                //SQL : select * from member where user_id = :userId
-//                //      and user_name = :userName order by id desc limit 5;
-//                memberRepository.findFirst5ByUserIdAndUserNameOrderByIdDesc(
-//                        "hong", "홍길동"
-//                );
-        Boolean isExist = memberRepository.existsByJoindateLessThanEqual(LocalDate.now());
-        System.out.println("isExist : " + isExist);
+        List<MemberEntity> list2 =
+                //SQL : select * from member where user_id = :userId
+                //      and user_name = :userName order by id desc limit 5;
+                memberRepository.findFirst5ByUserIdAndUserNameOrderByIdDesc(
+                        "hong", "홍길동"
+                );
+        assertEquals( 1, list2.size() );
+
+        Boolean isExist =
+                memberRepository.existsByJoindateLessThanEqual( LocalDate.now() );
+        System.out.println( "isExist : " + isExist );
 
         long count = memberRepository.countByUserNameIgnoreCaseLike("길");
-        System.out.println("count : "+ count);
-    }
+        System.out.println( "count : " + count);
 
+    }
     @Test
     @DisplayName("JPQL 테스트")
     public void jpqlQuery(){
-        List<MemberEntity> list = memberRepository.findByUserId_JPQL_Query("tom");
+        List<MemberEntity> list =
+                memberRepository.findByUserId_JPQL_Query("tom");
         assertEquals(1, list.size());
         assertEquals("톰아저씨", list.get(0).getUserName());
     }
-
     @Test
     @DisplayName("Native Query 테스트")
     public void nativeQuery(){
-        List<MemberEntity> list = memberRepository.findByUserId_nativeQuery("admin");
+        List<MemberEntity> list =
+                memberRepository.findByUserId_nativeQuery("admin");
         assertEquals(1, list.size());
         assertEquals("관리자", list.get(0).getUserName());
     }
-
     @Test
     @DisplayName("update 테스트")
     public void update(){
@@ -132,5 +197,40 @@ public class MemberRepositoryTest extends Ex12H2DbApplicationTests {
         }, () -> {
             fail("member 엔티티가 null입니다.");
         } );
+
     }
+    @Test
+    @DisplayName("update native SQL 테스트")
+    public void update_nativeSql(){
+        int result = memberRepository.updateById_nativeQuery(1L, "hong3");
+        assertEquals(1, result);
+
+        Optional<MemberEntity> member =
+                memberRepository.findById(1L);
+        assertEquals("hong3", member.get().getUserId() );
+    }
+
+    @Test
+    @DisplayName("delete 테스트")
+    public void delete(){
+        Optional<MemberEntity> member = memberRepository.findById(1L);
+        member.ifPresent(unwrapMember -> {
+            memberRepository.delete(unwrapMember);
+            System.out.println("삭제 성공");
+        });
+
+        Optional<MemberEntity> oldMember = memberRepository.findById(1L);
+        assertEquals(null, oldMember.get());
+    }
+
+//연습문제
+//아래와 같이 H2DB를 초기화 하고, 아래 결과를 출력하시오.
+
+//1. member 테이블 안에 암호가 "1234"인 회원이 있는지 테스트 하시오.
+//2. 23년도 3월에 가입한 회원의 수가 1인지 테스트하시오.
+//3. "lee"라는 아이디로 회원가입하고자 할때, 아이디 중복인지 테스트하시오.
+//4. "tom"이라는 아이디의 회원정보를 수정하고, 잘 수정되었는지 테스트하시오.
+//    톰아저씨 -> 마이클, 암호 -> 3456
+//* 쿼리 메소드 또는 Native SQL, JPQL 방법 중 하나를 사용하시오.
+
 }
